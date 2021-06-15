764d09f0fed56eb2f1f5098602f9c864cfb01070
Lorellana21 <lorellana.ordonez@gmail.com>
2021-06-15 20:33:46 +0200

    Add folder services with getdatafromApi and localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "../stylesheets/App.scss";
 import Header from "./Header";
 import Footer from "./Footer";
+import getDataFromApi from "../services/getDataFromApi";
 
 const App = () => {
   return (
