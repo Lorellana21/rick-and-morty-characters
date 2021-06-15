302fd6150f5ba2273960551f3250a23cc7ecece9
Lorellana21 <lorellana.ordonez@gmail.com>
2021-06-15 20:28:01 +0200

    Add footer and footer scss

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "../stylesheets/App.scss";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const App = () => {
-  return <Header></Header>;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../stylesheets/layout/_footer.scss";
+
+const Footer = (props) => {
+  return (
+    <div className="footer">
+      <span className="footer__text">
+        Made by Lorena Orellana &copy;Adalab 2021
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
