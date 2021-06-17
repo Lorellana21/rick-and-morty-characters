a48afbf26e89f26db5b3724995d30404c227f7e5
Lorellana21 <lorellana.ordonez@gmail.com>
2021-06-17 18:20:44 +0200

    add character not found, at last

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -3,9 +3,17 @@ import CharacterCard from "./CharacterCard";
 import PropTypes from "prop-types";
 
 const CharacterList = (props) => {
-  const characterElements = props.characters.map((character) => {
-    return <CharacterCard key={character.id} character={character} />;
-  });
+  let characterElements = "";
+
+  if (props.characters.length !== 0) {
+    characterElements = props.characters.map((character) => {
+      return <CharacterCard key={character.id} character={character} />;
+    });
+  } else {
+    characterElements = (
+      <h2 className="character__notfound">Sorry, character not found!</h2>
+    );
+  }
 
   return (
     <section className="character__section">
